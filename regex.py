9866db946a7f6ba96720b34c910ab3cd5a4f6267
timothysihombing <31738309+timothysihombing@users.noreply.github.com>
2018-04-20 23:51:42 +0700

    Add files via upload

---
+++ regex.py
@@ -0,0 +1,22 @@
+import re
+import tweepy
+
+consumer_key =  "i5qxQwsMYZOC0ibF5LZVbQ19G"
+consumer_secret = "97WmrQgYQqkGdJiDiUp8E5q5xqtR9mvmKo59gxGuPmFL33Y3bJ"
+access_token = "731777689028153344-fZu83uU9I2YnokAcbReFaV4wtWO9Z4H"
+access_token_secret = "RVgBZU6FPP7wFDWPzcHCVWiOYOqhebQbiHerSF3qrSNTw"
+
+auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(access_token, access_token_secret)
+
+api = tweepy.API(auth, wait_on_rate_limit=True)
+    
+if __name__ == "__main__":
+    public_tweets = api.home_timeline()
+    find_string = input('Masukkan string uji: ')
+    for tweet in public_tweets:
+        print (tweet.text)
+        m = re.findall(find_string, tweet.text)
+        if (len(m) != 0):
+            print("Spam detected!")
+        print ("---------------------------------------------------------------")
